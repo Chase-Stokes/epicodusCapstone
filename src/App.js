1ce712bd99101a6b909cabe170a0e4b669eb0473
chase-stokes <chasehstokes@gmail.com>
2022-05-13 15:37:06 -0700

    add route butbix route not working

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React from 'react';
-import './default.scss';
+import { Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
-import Homepage from './pages/Homepage/index';
+import Homepage from './pages/Homepage';
+import Registration from './pages/Registration';
+import './default.scss';
 
 function App() {
   return (
     <div className='App'>
       <Header />
       <div className="main">
-        <Homepage />
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path='/registration' element={<Registration />} />
+        </Routes>
       </div>
     </div>
   );
---
+++ src/pages/Registration/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './styles.scss';
+
+class Registration extends Component {
+
+    render() {
+        return (
+            <div>
+                <h1>Registration Page</h1>
+            </div>
+        )
+    }
+}
+
+export default Registration;
